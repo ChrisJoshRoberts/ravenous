07a6d0abe406475d38c4b1cec2aab6f3a8a8cd82
ChrisJoshRoberts <37948203+ChrisJoshRoberts@users.noreply.github.com>
2023-12-21 17:08:27 +0200

    Added BusinessList Component (Hardcode)

---
+++ src/BusinessList.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Business from "./Business";
+import { business } from "./Business";
+
+
+function BusinessList() {
+    return (
+        <div className="list-container">
+        <Business />
+        <Business />
+        </div>
+    )
+
+};
+export default BusinessList;
--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Business from './Business';
+import BusinessList from './BusinessList';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Business />
+    <BusinessList />
     
   </React.StrictMode>
 );
